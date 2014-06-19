c41e51f579884ab53e22bb218caafdd7418604b9
David Jordan <dbj8@cornell.edu>
2014-06-19 17:06:56 -0500

    Problem 4 (can't see that there are 3 sections because they are the same color)

--- DBJTableViewController.m
+++ DBJTableViewController.m
@@ -44,28 +44,32 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    if(section == 0){
+        return 2;
+    }else if(section == 1){
+        return 1;
+    }else if(section == 2)
+        return 3;
+    
+    //unreachable currently
+    return 1;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.textLabel.text = @"Cell";
     // Configure the cell...
     
     return cell;
 }
-*/
 
 /*
 // Override to support conditional editing of the table view.
